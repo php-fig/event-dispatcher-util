f5fb044a221b2cd8252fc6138ccaf9ac1d7e21b4
Larry Garfield <larry@garfieldtech.com>
2018-08-19 17:33:59 -0500

    Add utility code.

---
+++ src/CompoundProvider.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace Fig\EventDispatcher;
+
+
+use Psr\EventDispatcher\EventInterface;
+use Psr\EventDispatcher\ListenerProviderInterface;
+
+/**
+ * A compound provider encapsulates multiple other providers and concatenates their responses.
+ *
+ * Be aware that any ordering of listeners in different sub-providers is ignored, and providers are
+ * iterated in the order in which they were added.  That is, all listeners from the first provider
+ * added will be returned to the caller, then all listeners from the second provider, and so on.
+ */
+class CompoundProvider implements ListenerProviderInterface
+{
+    /**
+     * @var array
+     */
+    protected $providers = [];
+
+    public function getListenersForEvent(EventInterface $event): iterable
+    {
+        /** @var ListenerProviderInterface $provider */
+        foreach ($this->providers as $provider) {
+            yield from $provider->getListenersForEvent($event);
+        }
+    }
+
+    /**
+     * Enqueues a listener provider to this set.
+     *
+     * @param ListenerProviderInterface $provider
+     *   The provider to add.
+     * @return CompoundProvider
+     *   The called object.
+     */
+    public function addProvider(ListenerProviderInterface $provider) : self
+    {
+        $this->providers[] = $provider;
+        return $this;
+    }
+}
---
+++ src/StoppableTaskTrait.php
@@ -0,0 +1,22 @@
+<?php
+declare(strict_types=1);
+
+namespace Fig\EventDispatcher;
+
+use Psr\EventDispatcher\StoppableTaskInterface;
+
+trait StoppableTaskTrait
+{
+    protected $stop = false;
+
+    public function stopPropagation() : StoppableTaskInterface
+    {
+        $this->stop = true;
+        return $this;
+    }
+
+    public function isStopped() : bool
+    {
+        return $this->stop;
+    }
+}
