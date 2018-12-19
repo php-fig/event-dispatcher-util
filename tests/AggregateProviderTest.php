7e8f851c6a5cc14b76f184ad2a9086eceb4d8f6c
Larry Garfield <larry@garfieldtech.com>
2018-12-19 16:04:51 -0600

    Update for the latest changes to the spec.

--- tests/AggregateProviderTest.php
+++ tests/AggregateProviderTest.php
@@ -5,7 +5,6 @@ namespace Fig\EventDispatcher;
 
 
 use PHPUnit\Framework\TestCase;
-use Psr\EventDispatcher\EventInterface;
 use Psr\EventDispatcher\ListenerProviderInterface;
 
 class AggregateProviderTest extends TestCase
@@ -13,18 +12,18 @@ class AggregateProviderTest extends TestCase
     public function test_multiple_providers() : void
     {
         $provider1 = new class implements ListenerProviderInterface {
-            public function getListenersForEvent(EventInterface $event): iterable
+            public function getListenersForEvent(object $event): iterable
             {
-                yield function (CollectingTask $event) { $event->add('C'); };
-                yield function (CollectingTask $event) { $event->add('R'); };
+                yield function (CollectingEvent $event) { $event->add('C'); };
+                yield function (CollectingEvent $event) { $event->add('R'); };
             }
         };
         $provider2 = new class implements ListenerProviderInterface {
-            public function getListenersForEvent(EventInterface $event): iterable
+            public function getListenersForEvent(object $event): iterable
             {
-                yield function (CollectingTask $event) { $event->add('E'); };
-                yield function (CollectingTask $event) { $event->add('L'); };
-                yield function (CollectingTask $event) { $event->add('L'); };
+                yield function (CollectingEvent $event) { $event->add('E'); };
+                yield function (CollectingEvent $event) { $event->add('L'); };
+                yield function (CollectingEvent $event) { $event->add('L'); };
             }
         };
 
@@ -34,7 +33,7 @@ class AggregateProviderTest extends TestCase
         $p->addProvider($provider1)
           ->addProvider($provider2);
 
-        $event = new CollectingTask();
+        $event = new CollectingEvent();
 
         foreach ($p->getListenersForEvent($event) as $listener) {
             $listener($event);
